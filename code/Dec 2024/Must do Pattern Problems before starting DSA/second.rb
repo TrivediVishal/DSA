3cdf1847b316934ff44aec8024ee97528c67253d
TrivediVishal <104263742+TrivediVishal@users.noreply.github.com>
2024-12-15 08:36:36 +0000

    done with 3 patterns

---
+++ code/Dec 2024/Must do Pattern Problems before starting DSA/second.rb
@@ -0,0 +1,5 @@
+n = 5
+
+1.upto 5 do |i|
+  puts '*'*i
+end
---
+++ code/Dec 2024/Must do Pattern Problems before starting DSA/third.rb
@@ -0,0 +1,17 @@
+# n = 5
+
+# name = 'Vishal'
+
+# num = '123'
+
+# puts name[0..0]
+# print num[0..1]
+
+n = 5
+
+1.upto n do |i|
+  1.upto i do |j|
+     print j
+  end
+  puts ""
+end
